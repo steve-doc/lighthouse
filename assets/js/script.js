24198dd4beec01fdbfeb2c35d2ed084331c8ed25
stevedoc67 <sdoc67@gmail.com>
2023-07-19 16:17:27 +0100

    Added Hero section with Swiper api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,41 @@
+
+// Initialise Swiper
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  spaceBetween: 0,
+  effect: 'fade',
+  speed: 2000,
+  fadeEffect: {
+    crossFade: true
+  },
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  // For autoplay
+  autoplay: {
+    el: '.autoplay'
+  }
+});
+
+
+// set up intersection observers
+
 const navToggle = document.querySelector(".nav-toggle");
 const nav = document.querySelector('.nav');
 
@@ -6,8 +44,8 @@ navToggle.addEventListener('click', () => {
 })
 
 const sections = document.querySelectorAll('section');
+const faders = document.querySelectorAll('.fade-in');
 
-console.log(sections);
 
 const options = { 
   root: null,
@@ -21,10 +59,34 @@ const observer = new IntersectionObserver(function(entries, observer) {
       return;
     }
     console.log(entry.target);
-    entry.target.classList.toggle('inverse')
+    // entry.target.classList.toggle('inverse')
   })
 }, options);
 
 sections.forEach(section => {
   observer.observe(section);
 });
+
+const appearOptions = {
+  threshold: 1,
+  rootMargin: "0px 0px -50px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver
+(function(
+  entries,
+   appearOnScroll
+   ) {
+    entries.forEach(entry => {
+      if(!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add('appear');
+        appearOnScroll.unobserve(entry.target);
+      } 
+    })
+   }, appearOptions);
+
+   faders.forEach(fader => {
+    appearOnScroll.observe(fader)
+   })
